fc9734bc1b533256db967f161a16b4e0903711ac
Austin Joiner <austin.j.joiner1@gmail.com>
2023-05-18 12:06:43 -0400

    finished controllers exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,42 +1,55 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Post;
 import org.apache.coyote.Response;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
+
     public String get(Model model) {
-        model.addAttribute("title", "posts");
-        return "posts index page";
+        ArrayList<Post> posts = new ArrayList<>();
+
+        Post postOne = new Post("Title One, BODY ONE");
+        Post postTwo = new Post("Title Two, BODY TWO");
+
+        posts.add(postOne);
+        posts.add(postTwo);
+
+        model.addAttribute("posts", posts);
+
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String getId(Model model) {
-        model.addAttribute("title", "posts-ID");
-        return "view an individual post";
+//    @ResponseBody
+    public String getId(@PathVariable int id, Model model) {
+        Post post = new Post("post", "This is a individual post");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String getCreate(Model model) {
-        model.addAttribute("title", "posts-create");
+    public String getCreate() {
+
         return "view the form for creating a post";
     }
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String postCreate(Model model) {
-        model.addAttribute("title", "posts-create");
+    public String postCreate() {
+
         return "create a new post";
     }
-
 }
---
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -0,0 +1,36 @@
+package com.codeup.codeupspringblog.models;
+
+import java.util.Objects;
+
+public class Post {
+
+private String title;
+private String body;
+
+
+    public Post(String title) {
+        this.title = title;
+    }
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
